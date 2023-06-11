d747e8633b9da949037a6398003983c96897c587
Alice Kindheart <42279721+ak4338@users.noreply.github.com>
2023-06-11 19:23:58 -0400

    getting started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlaygroundsComponent } from './playgrounds/playgrounds.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path:"", component: PlaygroundsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PlaygroundsComponent } from './playgrounds/playgrounds.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlaygroundsComponent
   ],
   imports: [
     BrowserModule,
